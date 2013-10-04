5e65147075522411fa3b4e8b8356d73900ee0928
Manasi Havaldar <hmanasi@gmail.com>
2013-10-03 23:44:00 -0700

    Add initial version of bootstrap code

---
+++ public/javascripts/recipes/index.js
@@ -0,0 +1,18 @@
+(function () {
+    
+	deleteClicked = function (e)
+	{
+		var $deleteForm = $('#deleteForm-'+$(this).attr('recipeId'));
+		$deleteForm.submit();
+	}
+	
+	editClicked = function (e)
+	{
+		var $editForm = $('#editForm-'+$(this).attr('recipeId'));
+		$editForm.submit();
+	}
+	
+	$("[name=delete-button]").click (deleteClicked);
+	$("[name=edit-button]").click (editClicked);
+	
+}());
