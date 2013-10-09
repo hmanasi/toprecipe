2e8cc0b9e2d0b69d67d77fff43758831b3ce6673
Manasi Havaldar <hmanasi@gmail.com>
2013-10-08 22:44:26 -0700

    begin working on display fooditem in given category

--- app/com/toprecipe/controllers/CategoryBrowser.java
+++ app/com/toprecipe/controllers/CategoryBrowser.java
@@ -1,5 +1,24 @@
 package com.toprecipe.controllers;
 
-public class CategoryBrowser {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import com.toprecipe.models.Recipe;
+import com.toprecipe.repository.RecipeRepository;
+
+@org.springframework.stereotype.Controller
+public class CategoryBrowser extends Controller  {
+	
+	@Autowired
+	RecipeRepository repo;
+	
+	public Result foodItems(String categoryTitle) {
+		return TODO;//ok(views.html.recipes.index.render(repo.));
+	}
+	
+	
 
 }
