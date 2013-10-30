b55837dfb08c0d3e68efde47c994731a35c652d8
Manasi Havaldar <hmanasi@gmail.com>
2013-10-29 23:41:39 -0700

    Add link to original site

--- public/javascripts/fooditems/index.js
+++ public/javascripts/fooditems/index.js
@@ -3,15 +3,18 @@
 	recipeinfoClicked = function (e) 
 	{
 		var recipe = recipes[$(this).attr('foodItemId')];
-		generateDialogMarkup (recipe.title, recipe.videoUrl);
+		generateDialogMarkup (recipe.title, recipe.videoUrl, recipe.sourceUrl);
 		$("#recipe-info-modal").modal();
 	};
 	
-	generateDialogMarkup = function (title, videoUrl) 
+	generateDialogMarkup = function (title, videoUrl, sourceUrl) 
 	{
 		var iframe = "<iframe width=\"537\" height=\"315\" src=\"" +videoUrl+ "\" frameborder=\"0\" allowfullscreen></iframe>";
+		var source = "<a href=\"" +sourceUrl+ "\" target= \"_blank\"><h4>Go to original site</h4></a>"
 		$("#recipe-title").html (title);
 		$("#iframe").html (iframe);
+		$("#source").html (source);
+		
 	}
 	
 	$("[name=recipe-info-button]").click (recipeinfoClicked);
